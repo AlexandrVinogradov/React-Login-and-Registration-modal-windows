c5f8047e536b13be6b05a53dce8d84ffe65b4849
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2019-11-23 17:28:50 +0300

    added html

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import EnterToAccountModal from './components/EnterToAccountModal/EnterToAccountModal';
-import Registration from './components/Registration/Registration';
+import Registration from './components/RegistrationModal/Registration';
 
 
 class App extends Component {
@@ -26,24 +26,48 @@ class App extends Component {
         <div id='EnterToAccountModal'></div>
         <div id='Registration'></div>
         <main>
-
           <button onClick={this.toggleEnerModal}>Личный кабинет</button>
           {this.state.isEnterOpen &&
             <EnterToAccountModal onClose={this.toggleEnerModal}>
               <h1>Личный кабинет</h1>
-              <button onClick={this.toggleRegistrationModal} >Зарегистрироваться</button>
+              <p>Email</p>
+              <input />
+              <p>Пароль</p>
+              <input />
+              <button>Войти</button>
+              <div>
+                <div>
+                  <p>Еще нет аккаунта?</p>
+                  <button onClick={this.toggleRegistrationModal}>Зарегистророваться</button>
+                </div>
+                <div>
+                  <p>Забыли пароль?</p>
+                  <button onClick={this.toggleEnerModal} >Восстановить</button>
+                </div>
+              </div>
             </EnterToAccountModal>
           }
-
 
           {this.state.isRegistrationOpen &&
             <Registration onClose={this.toggleRegistrationModal}>
               <h1>Регистрация</h1>
+              <p>
+              Регистрация занимает 30 секунд. <br/>После регистрации вы получите<br/> 7 дней бесплатного доступа.
+              </p>
+              <p>Email</p>
+              <input />
+              <p>Пароль</p>
+              <input />
+              <p>не менее 6 символов</p>
+              <div>
+                <input type='checkbox'/>
+                <p>Я принимаю условия <a href='#'>Соглашения</a> </p>
+              </div>
+              <button onClick={this.toggleEnerModal}>Зарегистрироваться</button>
+              <p>Уже есть аккаунт?</p>
               <button onClick={this.toggleEnerModal} >Войти в личный кабинет</button>
             </Registration>
           }
-
-
         </main>
       </div>
     );

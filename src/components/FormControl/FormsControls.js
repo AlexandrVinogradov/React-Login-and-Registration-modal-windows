648b3c0201ea010346985d702a2b87201bb9ee5b
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2019-12-06 11:57:31 +0300

    added control styles

--- src/components/FormControl/FormsControls.js
+++ src/components/FormControl/FormsControls.js
@@ -12,14 +12,13 @@ const FormControl = ({ input, meta: { touched, error }, children }) => {
     </div>
 }
 export const Textarea = (props) => {
-    const { input, meta, child, ...restProps } = props;
+    const { input, meta, child, ...restProps } = props; 
     return <FormControl {...props}><textarea {...input} {...restProps} className={s.textarea} /></FormControl>
 }
 export const Input = (props) => {
     const { input, meta, child, ...restProps } = props;
     return <FormControl {...props}><input {...input} {...restProps} /></FormControl>
 }
-// 90 28:00
 export const createField = (placeholder, name, validators, component, props = {}, text = "") => (
     <div>
         <Field placeholder={placeholder} name={name}
